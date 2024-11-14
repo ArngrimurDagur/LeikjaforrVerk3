3e0ecd1b79509374caa16a003f77f571bc9a6dc6
Arngrimur Dagur <111790903+ArngrimurDagur@users.noreply.github.com>
2024-11-14 19:37:46 +0000

    Add files via upload

---
+++ Shooting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public float speed = 4000f;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Debug.Log("skjOtttttttta");       
+           
+            //GameObject instBullet = Instantiate(bullet, transform.position, Quaternion.identity) as GameObject;
+            GameObject instBullet = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
+            Rigidbody instBulletRigidbody = instBullet.GetComponent<Rigidbody>();
+            instBulletRigidbody.AddForce(transform.forward * speed);
+            Destroy(instBullet, 0.5f);
+           
+        }
+    }
+}
---
+++ Takki.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Takki : MonoBehaviour
+{
+    private void Update()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void Byrja()
+    {
+        SceneManager.LoadScene(1);
+        Ovinur.health = 30;
+    }
+
+    public void Endir()
+    {
+        SceneManager.LoadScene(0);
+        Ovinur.health = 30;
+    }
+}
